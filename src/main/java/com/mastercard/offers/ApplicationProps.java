a6bfffbcd3bb6759e42e391b84210776d6bd02ab
Ishfaq <rathcoole786@gmail.com>
2020-04-15 22:23:36 +0100

    Property Loader

---
+++ src/main/java/com/mastercard/offers/ApplicationProps.java
@@ -0,0 +1,18 @@
+package  com.mastercard.offers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class ApplicationProps {
+
+    @Autowired
+    private Environment env;
+
+    public String getProperty(String pPropertyKey) {
+        return env.getProperty(pPropertyKey);
+    }
+} 
